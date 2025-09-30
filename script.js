e018921939f23259d1f37b196b466b7425dc0d40
Deep Dey - The FUTURE IITIAN 🎯 <199653739+deepdeyiitgn@users.noreply.github.com>
2025-09-30 10:33:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -368,5 +368,4 @@ if (!window.bgMusic) {
   document.addEventListener("keydown", startMusic);
 }
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2533086861741403"
-     crossorigin="anonymous"></script>
+
